6d045213e94705961db052c50c4653a75b367518
Churt Noel Santos <202110256@gordoncollege.edu.ph>
2025-03-22 10:43:33 +0800

    Added Pages and Remove Login and Dashboard

---
+++ app/successlist/page.tsx
@@ -0,0 +1,5 @@
+export default function SuccessRoute() {
+    return (
+        <h1>success list hahaha love u babe</h1>
+    )
+}
---
+++ app/supportedtechnology/page.tsx
@@ -0,0 +1,7 @@
+export default function SupportedRoute() {
+    return (
+        <div className="py-6 px-4">
+            <h1>supported iloveu</h1>
+        </div>
+    )
+}

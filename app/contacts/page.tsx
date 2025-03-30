68f5553124fc4d4a0051a05fa3c832e3170f98c9
Lara Jane Acar <ljyape@gmail.com>
2025-03-30 22:21:53 +0800

    jhah

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,7 +3,7 @@ import { Mail, Phone, MapPin, Facebook } from "lucide-react";
 export default function Contacts() {
     return (
         <div className="bg-gradient-to-b from-blue-200 to-red-200 py-14 px-4 sm:px-8 flex justify-center">
-            <div className="max-w-6xl bg-white p-6 sm:p-12 md:p-16 rounded-3xl shadow-2xl border-8 border-gray-200 w-full">
+            <div className="max-w-6xl bg-white p-6 sm:p-12 md:p-16 rounded-3xl">
                 {/* HEADER SECTION */}
                 <div className="text-center mb-6 md:mb-8">
                     <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900">Contact Us</h1>
